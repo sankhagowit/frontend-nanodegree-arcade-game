69f62bfde6281056f2bd83b33e08829aa6451cef
Bobby <bobby@sankhagowit.com>
2017-02-13 12:46:40 -0600

    Abstracted y coordinate into switch function part of class prototype. Now it will make collision function easier

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
-// Enemies our player must avoid
+/*  Enemies our player must avoid. Class takes 3 parameters. The initial column
+*   from 0 to 4, the initial row from 0 to 5, and the speed at which the Enemy
+*   automatically moves.
+*/
 var Enemy = function(initCol, initRow, speed) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    // starting x position. All bugs begin in first left column
-    // which I believe is px 101 from the render function in engine.js x and y
-    // values used to draw the game tiles. initCol for bugs should be 0.
-    this.x = initCol;
-    // Initial y position, need to pass Enemy class a row to begin on. bugs
-    // can only start on the stone which are rows 1, 2, and 3 (if top row is 0)
-    this.y = initRow;
+    this.col = initCol;
+    this.row = initRow;
+    // starting x position from the column. All bugs begin in first left column
+    // all columns are 101 px wide, thus to get canvas x position multiply column
+    // by 101 px
+    this.x = this.col * 101;
+    // Initial y position or the initial row to start on with row 0 being the
+    // first row rendered. Sprites are 101px wide, thus we take the row Number
+    // Enemies can only start on the stone which are rows 1, 2, and 3 (top row 0)
+    // It seems the px position to be aligned on a certain row changes depending
+    // on what row the sprite is on, thus I will use a switch statement
+    //this.y = initRow;
+    this.rowToYcoordinate();
     // Number of current enemies on the game board, will also be the index
     // number of this instantiated class thus the .numEnemies can be used
     // to remove this bug from the allEnemies array when it is off of the board
@@ -34,8 +43,34 @@ Enemy.prototype.update = function(dt) {
     }
 };
 
+Enemy.prototype.rowToYcoordinate = function() {
+  switch (this.row) {
+    case 0:
+      this.y = -20;
+      break;
+    case 1:
+      this.y = 60;
+      break;
+    case 2:
+      this.y = 146;
+      break;
+    case 3:
+      this.y = 228;
+      break;
+    case 4:
+      this.y = 312;
+      break;
+    case 5:
+      this.y = 390;
+      break;
+    default:
+      this.y = 146; // default to the third row
+  }
+}
+
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+    this.rowToYcoordinate();
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -59,12 +94,12 @@ Player.prototype.handleInput = function(keys){
 
 Player.prototype.update = function() {
   //will this reset the player update method?
-  if(player.y < playerRowpx){
-    player.x = playerColStart;
-    player.y = playerRowStart;
-  } else {
-    //change the x and y in accordance with the pressed keys
-  };
+  // if(player.y < playerRowpx){
+  //   player.x = playerColStart;
+  //   player.y = playerRowStart;
+  // } else {
+  //   //change the x and y in accordance with the pressed keys
+  // }
 }
 
 // Now instantiate your objects.
@@ -75,18 +110,12 @@ Player.prototype.update = function() {
 // make a helper function outside of enemy class to push the created enemy
 // into the allEnemies array.
 var allEnemies = [];
-// Create variables to transform bug row & col into px positions
-var enemyColpx = 101;
-var enemyRowpx = 73;
-allEnemies.push(new Enemy(0*enemyColpx,2*enemyRowpx,100));
+allEnemies.push(new Enemy(-1,2,100));
 
-// Create variables to transform player row & col to px positions
-var playerColpx = 101;
-var playerRowpx = 83;
-var playerColStart = 2*playerColpx;
-var playerRowStart = 5*playerRowpx;
+var playerColStart = 2;
+var playerRowStart = 5;
 
-var player = new Player(playerColStart,playerRowStart,100);
+var player = new Player(2,5,0);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
